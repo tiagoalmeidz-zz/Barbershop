9fde6d2eda9dd1b09ff35fa09d067f423938b6b3
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-09-25 20:08:37 -0300

    Criando loader de models

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Tiago Almeida' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Tiago Fernando de Almeida',
+    email: 'tiago.fernando.almeida@gmail.com',
+    password_hash: '1234',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
