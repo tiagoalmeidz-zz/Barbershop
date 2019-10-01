86c8d07d1d642f3bc5c11636d634f5a775a81da4
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-09-30 22:52:34 -0300

    Cadastro de Usuarios - UserController

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controller/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Tiago Fernando de Almeida',
-    email: 'tiago.fernando.almeida@gmail.com',
-    password_hash: '1234',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
